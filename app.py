663d70c8d7de3e06f02eb9bef97e10d577ee11b7
Pedro Estima Cardoso <root@comptable.maths.cam.ac.uk>
2019-02-14 12:07:54 +0000

    Adding the room allocating script

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import alloc_script
 from flask import Flask, jsonify, abort, make_response, request
 from config import app_config
 import json
@@ -27,8 +28,9 @@ def connectToDB():
     #print(conn_string)
     try:
         return psycopg2.connect(conn_string)
-    except:
+    except psycopg2.OperationalError as e:
         print("Can't connect to database")
+        print('Error message:\n{0}').format(e)
 
 def queryDB(query, args=()):
     conn = connectToDB()
@@ -39,7 +41,8 @@ def queryDB(query, args=()):
         cur.execute(query, args)
         r = [dict((cur.description[i][0], value) \
                for i, value in enumerate(row)) for row in cur.fetchall()]
-        #return (r[0] if r else None) if one else r
+        if r is None:
+            abort(404)
         return r
     except psycopg2.OperationalError as e:
         print("Error executing Statement")
@@ -51,40 +54,33 @@ def queryDB(query, args=()):
 def hello():
     return "Hello World!"
 
+
+@app.route('/alloc')
+def alloc():
+    return jsonify(alloc_script.alloc())
+
 @app.route('/rooms')
 @auth.login_required
 def get_rooms():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.rooms")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/rooms/pavC')
 @auth.login_required
 def get_rooms_pav_c():
     my_query = queryDB("""SELECT * FROM testing_schema_pedro.rooms WHERE "floorId" IN (430,431,432,433)""")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
     return jsonify(my_query);
 
 @app.route('/rooms/pavH')
 @auth.login_required
 def get_rooms_pav_h():
     my_query = queryDB("""SELECT * FROM testing_schema_pedro.rooms WHERE "floorId" IN (422,423,424,425)""")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
     return jsonify(my_query);
 
 @app.route('/rooms/<int:room_id>')
 @auth.login_required
 def get_room(room_id):
     my_query = queryDB("SELECT * FROM testing_schema_pedro.rooms WHERE id=%s", (room_id,))
-
-    if my_query is None:
-        abort(404)
     return jsonify(my_query);
 
 @app.route('/rooms/<int:room_id>', methods=['PUT'])
@@ -113,82 +109,55 @@ def update_room(room_id):
 @auth.login_required
 def get_room_computers(room_id):
     my_query = queryDB("""SELECT * FROM testing_schema_pedro.computers WHERE "roomId"=%s""", (room_id,))
-
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/rooms/<int:room_id>/persons')
 @auth.login_required
 def get_room_persons(room_id):
     my_query = queryDB("""SELECT * FROM testing_schema_pedro.persons WHERE "roomId"=%s""", (room_id,))
-
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/persons')
 @auth.login_required
 def get_persons():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.persons")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/persons/<int:person_id>')
 @auth.login_required
 def get_person(person_id):
     my_query = queryDB("SELECT * FROM testing_schema_pedro.persons WHERE id=%s", (person_id,))
-    #print(my_query)
-    if not my_query:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/computers')
 @auth.login_required
 def get_computers():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.computers")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/computers/pavC')
 @auth.login_required
 def get_computers_pav_c():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.computers_pavc")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/computers/pavH')
 @auth.login_required
 def get_computers_pav_h():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.computers_pavh")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/computers/<int:computer_id>')
 @auth.login_required
 def get_computer(computer_id):
     my_query = queryDB("SELECT * FROM testing_schema_pedro.computers WHERE id=%s", (computer_id,))
-
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 @app.route('/locations')
 @auth.login_required
 def get_locations():
     my_query = queryDB("SELECT * FROM testing_schema_pedro.locations")
-    #print(my_query)
-    if my_query is None:
-        abort(404)
-    return jsonify(my_query);
+    return jsonify(my_query)
 
 
 @app.route('/computers/<int:hardware_id>', methods=['PUT'])
