88037d2d24f85815540b516c5f558dc5c09ae9b1
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-03-14 17:14:47 +0000

    Resolve room number and room id

--- src/app/_resolvers/personsInRoom.resolver.ts
+++ src/app/_resolvers/personsInRoom.resolver.ts
@@ -14,6 +14,20 @@ export class PersonsInRoomResolver implements Resolve<Person[]> {
         private alertify: AlertifyService) {}
 
         resolve(route: ActivatedRouteSnapshot): Observable<Person[]> {
+            // check if the route has a numeric value. If it has then it should be a room id.
+            // If not it should be a room_number
+            const isnum = /^\d+$/.test(route.params['id']);
+
+            if (!isnum) {
+                return this.computerService.getPersonsInRoomNumber(route.params['id']).pipe(
+                    catchError(error => {
+                        this.alertify.error('Problem retrieving data.' + error.message);
+                        this.router.navigate(['/']);
+                        console.log(error);
+                        return of(null);
+                    })
+                );
+            }
             return this.computerService.getPersonsInRoom(route.params['id']).pipe(
                 catchError(error => {
                     this.alertify.error('Problem retrieving data.' + error.message);
--- src/app/_resolvers/room-detail.resolver.ts
+++ src/app/_resolvers/room-detail.resolver.ts
@@ -14,13 +14,30 @@ export class RoomDetailResolver implements Resolve<Room> {
         private alertify: AlertifyService) {}
 
         resolve(route: ActivatedRouteSnapshot): Observable<Room> {
-            return this.computerService.getRoom(route.params['id']).pipe(
-                catchError(error => {
-                    this.alertify.error('Problem retrieving data.' + error.message);
-                    this.router.navigate(['/']);
-                    console.log(error);
-                    return of(null);
-                })
-            );
+            // check if the route has a numeric value. If it has then it should be a room id.
+            // If not it should be a room_number
+            const isnum = /^\d+$/.test(route.params['id']);
+            console.log(isnum);
+
+            if (!isnum) {
+                return this.computerService.getRoomNumber(route.params['id']).pipe(
+                    catchError(error => {
+                        this.alertify.error('Problem retrieving data.' + error.message);
+                        this.router.navigate(['/']);
+                        console.log(error);
+                        return of(null);
+                    })
+                );
+            } else {
+                return this.computerService.getRoom(route.params['id']).pipe(
+                    catchError(error => {
+                        this.alertify.error('Problem retrieving data.' + error.message);
+                        this.router.navigate(['/']);
+                        console.log(error);
+                        return of(null);
+                    })
+                );
+            }
         }
+
 }
