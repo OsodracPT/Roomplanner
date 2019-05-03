e04a483483ae3aac682c19a94cc6e789006833aa
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-05-03 16:52:19 +0100

    Changing tabs adds fragments to the route

--- src/app/pavillions/pav-c/pav-c.component.ts
+++ src/app/pavillions/pav-c/pav-c.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { Computer } from 'src/app/_models/computer';
 import { Room } from 'src/app/_models/room';
 import { ComputerService } from 'src/app/services/computer.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 import { AlertifyService } from 'src/app/services/alertify.service';
-
+import {TabsetComponent} from 'ngx-bootstrap';
+import { TabDirective } from 'ngx-bootstrap/tabs';
 
 
 @Component({
@@ -16,13 +17,25 @@ import { AlertifyService } from 'src/app/services/alertify.service';
 })
 export class PavCComponent implements OnInit {
 
+  @ViewChild('pavillionTabs') pavillionTabs: TabsetComponent;
   computers: Computer[];
   rooms: Room[];
   pav_letter: string;
-  constructor(private computerService: ComputerService,  private router: Router, private alertify: AlertifyService) { }
+  constructor(private computerService: ComputerService,  private router: Router, 
+    private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
+    // Get fragment from the url and select the tab accordingly
+    this.route.fragment.subscribe(
+      (fragment) => { 
+        if(+fragment < 4 && +fragment > 0){
+          this.pavillionTabs.tabs[fragment].active = true;
+        }
+      }
+    );
+
+    // Get the pavillion letter from the URL
     this.pav_letter = this.router.url.substr(4, 1).toUpperCase();
 
     this.computerService.getPavRooms(this.pav_letter)
@@ -42,6 +55,11 @@ export class PavCComponent implements OnInit {
       // console.log(numberOfComputers.number_of_computers);
       return numberOfComputers.number_of_computers;
     }
+  }
+
+  // Addding a fragment to the URL when the user changes tabs
+  onSelect(data: TabDirective): void {
+  this.router.navigate( ['.'], { relativeTo: this.route, fragment: data.id});
   }
   }
 
