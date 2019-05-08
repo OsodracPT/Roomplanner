66435c7974302686fb57197d80f4f1f1a80825c7
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-05-08 17:29:08 +0100

    The /persons route now supports id and crsid

--- src/app/_resolvers/person-detail.resolver.ts
+++ src/app/_resolvers/person-detail.resolver.ts
@@ -14,14 +14,31 @@ export class PersonDetailResolver implements Resolve<Person> {
         private alertify: AlertifyService) {}
 
         resolve(route: ActivatedRouteSnapshot): Observable<Person> {
-            return this.computerService.getPerson(route.params['id']).pipe(
-                catchError(error => {
-                    this.alertify.error('Problem retrieving data from the API.' + error.error.api);
-                    this.alertify.error('Sending you back to the main page.');
-                    this.router.navigate(['/']);
-                    console.log(error);
-                    return of(null);
-                })
-            );
+            // check if the route has a numeric value. If it has then it should be a person id.
+            // If not it should be a crsid
+            const isnum = /^\d+$/.test(route.params['id']);
+            console.log(isnum);
+
+            if (!isnum) {
+                return this.computerService.getPersonCrsid(route.params['id']).pipe(
+                    catchError(error => {
+                        this.alertify.error('Problem retrieving data from the API. ' + error.message);
+                        this.alertify.error('Sending you back to the main page.');
+                        this.router.navigate(['/']);
+                        console.log(error);
+                        return of(null);
+                    })
+                );
+            } else {
+                return this.computerService.getPerson(route.params['id']).pipe(
+                    catchError(error => {
+                        this.alertify.error('Problem retrieving data from the API.' + error.error.api);
+                        this.alertify.error('Sending you back to the main page.');
+                        this.router.navigate(['/']);
+                        console.log(error);
+                        return of(null);
+                    })
+                );
+            }
         }
 }
