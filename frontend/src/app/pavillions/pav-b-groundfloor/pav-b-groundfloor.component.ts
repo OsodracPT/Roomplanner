5c561ee00a940518184539f59610d3fe8b7e26d4
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-03-05 02:00:46 +0000

    Handler for undefined room in PavB

--- frontend/src/app/pavillions/pav-b-groundfloor/pav-b-groundfloor.component.ts
+++ frontend/src/app/pavillions/pav-b-groundfloor/pav-b-groundfloor.component.ts
@@ -1,41 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Room } from 'src/app/_models/room';
+import { Component, OnInit, Input } from "@angular/core";
+import { Room } from "src/app/_models/room";
 
 @Component({
-  selector: 'app-pav-b-groundfloor',
-  templateUrl: './pav-b-groundfloor.component.html',
-  styleUrls: ['./pav-b-groundfloor.component.css']
+  selector: "app-pav-b-groundfloor",
+  templateUrl: "./pav-b-groundfloor.component.html",
+  styleUrls: ["./pav-b-groundfloor.component.css"]
 })
 export class PavBGroundfloorComponent implements OnInit {
   @Input() rooms: Room[];
   @Input() pav_letter: string;
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  getNumberOfComputer(roomNumber: string){
+  getNumberOfComputer(roomNumber: string) {
     //Preventing the method from executing before ngOnInit
-    if(this.rooms)
-    {
+    if (this.rooms) {
       roomNumber = this.pav_letter + roomNumber;
       var room = this.rooms.find(x => x.room_number == roomNumber);
       // console.log(numberOfComputers.number_of_computers);
-      return room.number_of_computers;
+      return typeof room !== "undefined" ? room.number_of_computers : 0;
     }
   }
 
-  getNumberOfPeople(roomNumber: string){
+  getNumberOfPeople(roomNumber: string) {
     //Preventing the method from executing before ngOnInit
-    if(this.rooms)
-    {
+    if (this.rooms) {
       roomNumber = this.pav_letter + roomNumber;
       var room = this.rooms.find(x => x.room_number == roomNumber);
-      // console.log(numberOfComputers.number_of_computers);
-      return room.number_of_people;
+      return typeof room !== "undefined" ? room.number_of_people : 0;
     }
   }
-
 }
