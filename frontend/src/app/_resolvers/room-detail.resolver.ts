06d5acedadfbf1779cd4e6cf82929f05e90eab2f
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-03-05 02:09:38 +0000

    Improved error handling

--- frontend/src/app/_resolvers/room-detail.resolver.ts
+++ frontend/src/app/_resolvers/room-detail.resolver.ts
@@ -1,45 +1,49 @@
-import { catchError } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
-import { ComputerService } from '../services/computer.service';
-import { AlertifyService } from '../services/alertify.service';
-import { Room } from '../_models/room';
+import { catchError } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Resolve, Router, ActivatedRouteSnapshot } from "@angular/router";
+import { ComputerService } from "../services/computer.service";
+import { AlertifyService } from "../services/alertify.service";
+import { Room } from "../_models/room";
 
 @Injectable()
-
 export class RoomDetailResolver implements Resolve<Room> {
-    constructor(private computerService: ComputerService,
-        private router: Router,
-        private alertify: AlertifyService) {}
+  constructor(
+    private computerService: ComputerService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
 
-        resolve(route: ActivatedRouteSnapshot): Observable<Room> {
-            // check if the route has a numeric value. If it has then it should be a room id.
-            // If not it should be a room_number
-            const isnum = /^\d+$/.test(route.params['id']);
-            console.log(isnum);
+  resolve(route: ActivatedRouteSnapshot): Observable<Room> {
+    // check if the route has a numeric value. If it has then it should be a room id.
+    // If not it should be a room_number
+    const isnum = /^\d+$/.test(route.params["id"]);
+    //console.log(isnum);
 
-            if (!isnum) {
-                return this.computerService.getRoomNumber(route.params['id']).pipe(
-                    catchError(error => {
-                        this.alertify.error('Problem retrieving data from the API. ' + error.message);
-                        this.alertify.error('Sending you back to the main page.');
-                        this.router.navigate(['/']);
-                        console.log(error);
-                        return of(null);
-                    })
-                );
-            } else {
-                return this.computerService.getRoom(route.params['id']).pipe(
-                    catchError(error => {
-                        this.alertify.error('Problem retrieving data from the API. ' + error.message);
-                        this.alertify.error('Sending you back to the main page.');
-                        this.router.navigate(['/']);
-                        console.log(error);
-                        return of(null);
-                    })
-                );
-            }
-        }
-
+    if (!isnum) {
+      return this.computerService.getRoomNumber(route.params["id"]).pipe(
+        catchError(error => {
+          this.alertify.error(
+            "Problem retrieving data from the API. " + error.message
+          );
+          //this.alertify.error("Sending you back to the main page.");
+          this.router.navigate(["/404"]);
+          console.log(error);
+          return of(null);
+        })
+      );
+    } else {
+      return this.computerService.getRoom(route.params["id"]).pipe(
+        catchError(error => {
+          this.alertify.error(
+            "Problem retrieving data from the API. " + error.message
+          );
+          //this.alertify.error("Sending you back to the main page.");
+          this.router.navigate(["/404"]);
+          console.log(error);
+          return of(null);
+        })
+      );
+    }
+  }
 }
