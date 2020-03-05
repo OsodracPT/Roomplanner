009190f04d5f800a35e52622a4ac9415fbfd5448
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-03-05 02:00:46 +0000

    Handler for undefined room in PavB

--- frontend/src/app/pavillions/pav-b-lower/pav-b-lower.component.ts
+++ frontend/src/app/pavillions/pav-b-lower/pav-b-lower.component.ts
@@ -29,7 +29,6 @@ export class PavBLowerComponent implements OnInit {
     if (this.rooms) {
       roomNumber = this.pav_letter + roomNumber;
       var room = this.rooms.find(x => x.room_number == roomNumber);
-      // console.log(numberOfComputers.number_of_computers);
       return typeof room !== "undefined" ? room.number_of_people : 0;
     }
   }
